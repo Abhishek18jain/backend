24d351a0d9fe219d4ba2ae9b46516904d2f38162
jainshab1803 <abhijain1803g@gmail.com>
2025-12-01 00:33:46 +0530

    malik

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+// const cors = require("cors");
 const fileUpload = require("express-fileupload");
 require("dotenv").config();
 
@@ -12,10 +12,17 @@ const app = express();
 // Railway gives PORT automatically
 const PORT = process.env.PORT || 5000;
 
+const cors = require("cors");
+
 app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST"],
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: false
 }));
+
+app.options("*", cors());
+
 
 app.use(express.json());
 app.use(fileUpload());
